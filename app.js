e08738d9adf552d890ff6c85d49a20fa56874a70
RenanJPaula <renanjohannsen@hotmail.com>
2015-12-27 00:36:38 -0200

    confecção do material de javascript

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-
-(function teste() {
-  a = 10;
-})();
-
+var a;
+a = 2;
 console.log(a);
+
+function hoistingExp() {
+    var a;
+    console.log(a);
+    a = 3;
+    console.log(a);
+}
+
+hoistingExp();
