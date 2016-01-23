90f5318bf55fd2375712515cd7a6f9e71180110c
renanjpaula <renanjohannsen@hotmail.com>
2016-01-23 10:34:08 -0200

    Criação do projeto base para fixação do conteúdo de JavaScript

--- academy-manager/js/student-controller.js
+++ academy-manager/js/student-controller.js
@@ -1,1 +1,10 @@
-console.log('teste');
+// Criar uma função construtora para ã instanciação de Estudantes que contenha
+// os seguintes campos:
+// name
+// phone
+// mail
+// address
+
+// document.getElementById('salvar').addEventListener('click', callback)
+// document.getElementById('nome').value
+// alert('teste');
